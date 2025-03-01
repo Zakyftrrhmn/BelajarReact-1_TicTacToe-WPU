7873be835e5dee970870248e8d5d014d2ac25efe
Zakyftrrhmn <zakifaturrahman0@gmail.com>
2025-03-01 09:16:16 +0700

    Creating game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState("");
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="board">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
